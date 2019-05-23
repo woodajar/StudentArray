1dd429cfc88002d8e914ee50a6a5abc8acd9e1e8
wooda <wooda@LAPTOP-RCKPHMJ6>
2019-05-23 09:44:01 -0400

    New branch

---
+++ src/syst17796/PartTimeStudent.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package syst17796;
+
+/**
+ *
+ * @author wooda
+ */
+public class PartTimeStudent extends Student {
+    
+}
